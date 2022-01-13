264843a1447a719097a8f4085363219a87e9d61d
bobopl360 <bobobo468@gmail.com>
2022-01-13 02:07:08 +0200

    Add Swagger

--- CarService/Program.cs
+++ CarService/Program.cs
@@ -17,7 +17,7 @@ namespace CarService
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
